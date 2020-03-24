8523993ea3b5db9704c92c84988a362a351e7204
robin <34326615+robin1577@users.noreply.github.com>
2020-03-25 00:40:42 +0800

    1

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,12 @@
 import os
-listdir=os.listdir("D:/webshell-detect/samples_data/WordPress")
-print(listdir)
+#listdir=os.listdir("D:/webshell-detect/samples_data/WordPress")
+dir_path="D:/webshell-detect/samples_data/WordPress"
+extension="php"
+target_path=""
+
+def copyfile():
+    for root,dirs,files in os.walk():
+        for file in files:
+            ext=os.path.splitext(file)[1][1:]
+            if ext==extension:
+                
