aaff274781c5f636bed4e237e4772b1530b43f67
robin <34326615+robin1577@users.noreply.github.com>
2020-03-25 22:48:08 +0800

    plt

--- webshell_detect.py
+++ webshell_detect.py
@@ -1,8 +1,13 @@
 #codind:utf-8
 import os
-#from keras import models
-#from keras import layers
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras import models,layers
 path="D:\webshell-detect\php\phptrain"
+max_len=500#每一个文件最大读入500个单词
+max_words=10000#字典最大个数
 def file_to_str(name):
     string=""
     with open(name,'r',encoding='ISO-8859-1') as f:
@@ -29,5 +34,55 @@ def files_to_str(path):
     print("True:",len(t))
     print("Talse:",len(f))
     return(t+f,tlabel+flabel)
-trains,labels=files_to_str(path)
+def main():
+    #生成字典
+    pdata,plabels=files_to_str(path)
+    tokenizer=Tokenizer(max_words)#字典个数
+    tokenizer.fit_on_texts(pdata)
+    word_index=tokenizer.index_word
+    print(f"字典个数{len(word_index)}")
+    #文本转序列
+    sequences=tokenizer.texts_to_sequences(pdata)
+    #生成数据标签
+    data=pad_sequences(sequences,maxlen=max_len)
+    labels=np.asarray(layers)
+    #打乱数据标签
+    indices=np.arange(data.shape[0])
+    np.random.shuffle(indices)
+    data=data[indices:]
+    labels=labels[indices]
+    print("data shape: ",data.shape)
+    print("labes shape:",labels.shape)
+    #划为数据为训练,验证，测试 6:2:2
+    x_train=data[:1020]
+    y_train=labels[:1020]
+    x_val=data[1020:1360]
+    y_val=labels[1020:1360]
+    x_test=data[1360:]
+    y_test=labels[1360:]
+    #构建网络
+    model=models.Sequential()
+    model.add(layers.Embedding(max_words,32,input_length=max_len))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(32,activation=""))
+    model.add(layers.Dense(1,activation="sigmoid"))
+    #编译并训练模型
+    model.compile(optimizer="rmsprop",loss='binary_crossentropy',metrics=['acc'])
+    history=model.fit(x_train,y_train,batch_size=128,epochs=5,validation_data=(x_val,y_val))
+    model.save_weights("embed_model.h5")
+    #history
+    acc=history.history['acc']
+    val_acc=history.history['val_acc']
+    loss=history.history['loss']
+    val_loss=history.history['val_loss']
+    epochs=range(1,len(acc)+1)
+    plt.plot(epochs,acc,'bo',label="Train acc")
+
+    #绘制结果
+    import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    main()
+
+
+
  
