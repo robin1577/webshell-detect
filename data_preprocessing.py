e721161723177a2f91d0f3858deacb3202f08f53
robin <34326615+robin1577@users.noreply.github.com>
2020-03-25 08:39:23 +0800

    over

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,17 +1,20 @@
 import os
 import shutil
 #listdir=os.listdir("D:/webshell-detect/samples_data/WordPress")
-data=[["D:/webshell-detect/samples_data/webshell/php/","D:/webshell-detect/php/phptrain/" ,"t",".php"] ,\
-    ["D:/webshell-detect/samples_data/WordPress/","D:/webshell-detect/php/phptrain/","F",".php"]]
-
+data=[["D:/webshell-detect/samples_data/webshell","D:/webshell-detect/php/phptrain" ,"ta",".php"] ,\
+    ["D:/webshell-detect/samples_data/WordPress","D:/webshell-detect/php/phptrain","F",".php"],\
+    ["D:/webshell-detect/samples_data/Webshell2","D:/webshell-detect/php/phptrain" ,"tb",".php"],\
+    ["D:/webshell-detect/samples_data/php-webshells","D:/webshell-detect/php/phptrain" ,"tc",".php"]]
 def copyfile(dir_path,target_path,name,extension):
     i=0
     for root,dirs,files in os.walk(dir_path):
         for file in files:
             ext=os.path.splitext(file)[1]
             if ext==extension:
-                shutil.copy(root+file,target_path+name+str(i)+extension)
+                shutil.copy(os.path.join(root,file),os.path.join(target_path,name+str(i)+extension))
                 i+=1
+                if i>999:
+                    break
 def main():
     for sample in data:
         copyfile(sample[0],sample[1],sample[2],sample[3])
